2a29fc12271d952f6b3a47ec23d0c85da6b6a81e
aduarte95 <aduarte.roldan@gmail.com>
2019-06-20 20:01:28 -0600

    Initial app

---
+++ Elabs/Elabs/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Elabs.Startup))]
+namespace Elabs
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
